0f5cf3101f4f0bb6f824f1bc0bc6547083e6a595
Hao Wang <ge_nah@hotmail.com>
2019-07-21 21:13:24 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -41,11 +41,15 @@ function createQuoteElements(quote, likesNumber) {
   const button2 = document.createElement('button');
   button2.className = 'btn-danger';
   button2.innerText = 'Delete';
+  const button3 = document.createElement('button');
+  button3.className = 'btn-success';
+  button3.innerText = 'Edit';
   blockquote.appendChild(p);
   blockquote.appendChild(footer);
   blockquote.appendChild(br);
   blockquote.appendChild(button1);
   blockquote.appendChild(button2);
+  blockquote.appendChild(button3);
   li.appendChild(blockquote);
   ul.appendChild(li);
   //add eventlistener to delete button
@@ -59,7 +63,35 @@ function createQuoteElements(quote, likesNumber) {
     likesNumber += 1
     span.innerText = likesNumber;
   })
+  //add eventlistener to edit button
+  button3.addEventListener('click', function(e) {
+    createEditForm(blockquote, author, content, quoteId, p, footer);
+    // fetchAddLike(quoteId);
+  })
 }
+
+function createEditForm(blockquote, author, content, quoteId, p, footer) {
+  const form = document.createElement('form');
+  const authorInput = document.createElement('input');
+  authorInput.type = 'text';
+  authorInput.placeholder = author;
+  const quoteInput = document.createElement('input');
+  quoteInput.type = 'text';
+  quoteInput.placeholder = content;
+  const submitEditButton = document.createElement('button');
+  submitEditButton.type = 'submit';
+  submitEditButton.innerText = 'Submit';
+  form.appendChild(authorInput);
+  form.appendChild(quoteInput);
+  form.appendChild(submitEditButton);
+  blockquote.appendChild(form);
+  //add eventlistener to submit edit button
+  submitEditButton.addEventListener('click', function(e) {
+     fetchUpdateData(blockquote, quoteInput, authorInput, quoteId, p, footer);
+  })
+}
+
+
 //add event listener to submit button to create new quote
 function newQuote() {
   const submitButton = document.querySelector("button[type='submit']");
@@ -98,6 +130,28 @@ function fetchDeleteData(quoteId) {
     method: 'DELETE'
   })
   .then(response => response.json())
+}
+
+function fetchUpdateData(blockquote, quoteInput, authorInput, quoteId, p, footer) {
+  return fetch(`http://localhost:3000/quotes/${quoteId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      id: quoteId,
+      quote: quoteInput.value,
+      author: authorInput.value
+    })
+  })
+  .then(response => response.json())
+  .then(json => udpateQuoteData(blockquote, quoteInput, authorInput, quoteId, p, footer))
+}
+
+function udpateQuoteData(blockquote, quoteInput, authorInput, quoteId, p, footer) {
+  footer.innerText = authorInput.value;
+  p.innerText = quoteInput.value;
 }
 
 function fetchAddLike(quoteId) {
